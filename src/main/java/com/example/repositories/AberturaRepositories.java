a86e6324b287ce94d55ab7d9200a219a6d0be76e
BrunoM <maximino.bruno92@gmail.com>
2021-11-02 17:59:56 -0300

    rompiendo todo de nuevo

--- src/main/java/com/example/repositories/AberturaRepositories.java
+++ src/main/java/com/example/repositories/AberturaRepositories.java
@@ -1,9 +1,7 @@
 package com.example.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.entities.Abertura;
 import com.example.entities.ClaseBase;
 
 @Repository
