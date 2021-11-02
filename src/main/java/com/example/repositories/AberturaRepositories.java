cc8388fce0e0cd039e90ae7b4bc0914083ef4cb5
Sebastian Zonta <seba.zonta@gmail.com>
2021-11-02 19:15:10 -0300

    intentando arreglar todo

--- src/main/java/com/example/repositories/AberturaRepositories.java
+++ src/main/java/com/example/repositories/AberturaRepositories.java
@@ -1,10 +1,12 @@
 package com.example.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.entities.Abertura;
 import com.example.entities.ClaseBase;
 
 @Repository
-public interface AberturaRepositories<TClass extends ClaseBase,TID> extends BaseRepository<ClaseBase, TID> {
+public interface AberturaRepositories extends JpaRepository<Abertura, Integer> {
 	
 }
