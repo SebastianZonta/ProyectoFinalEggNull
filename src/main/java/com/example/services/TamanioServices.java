a0ee340620f3e88c31d5521d0ff242bd69a4df90
ezequiel <ezelopezdy@gmail.com>
2021-11-02 21:41:21 -0300

    terminado tamanio servicio

--- src/main/java/com/example/services/TamanioServices.java
+++ src/main/java/com/example/services/TamanioServices.java
@@ -1,5 +1,92 @@
 package com.example.services;
 
-public class TamanioServices {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.entities.Tamanio;
+
+import com.example.errores.WebException;
+import com.example.repositories.TamanioRepositories;
+
+
+
+@Service
+public class TamanioServices {
+ 
+	@Autowired
+	private TamanioRepositories rpsTamanio;
+	
+	 @Transactional
+	public void tamanio( String descripcion_tamanio, Integer tamanio, Integer tamanio_precio)throws WebException{
+		
+		validar(descripcion_tamanio, tamanio,tamanio_precio);
+		
+		Tamanio tam = new Tamanio();
+	
+	    tam.setDescripcion_tamanio(descripcion_tamanio);
+	    tam.setTamanio(tamanio);
+	    tam.setPrecio_tamanio(tamanio_precio);
+	    rpsTamanio.save(tam);
+
+}
+	
+	 @Transactional
+	public void modificar(Integer id , String descripcion_tamanio, Integer tamanio, Integer tamanio_precio)throws WebException {
+		
+		validar(descripcion_tamanio, tamanio,tamanio_precio);
+		
+		Optional<Tamanio> modificar = rpsTamanio.findById(id);
+		if(modificar.isPresent()) {
+			Tamanio tam = modificar.get();
+			  tam.setDescripcion_tamanio(descripcion_tamanio);
+			    tam.setTamanio(tamanio);
+			    tam.setPrecio_tamanio(tamanio_precio);
+			    rpsTamanio.save(tam);
+		}
+		
+	}
+	 @Transactional
+	public void desahibilitar(Integer id, boolean alta) {
+		Optional<Tamanio> modificar = rpsTamanio.findById(id);
+		if(modificar.isPresent()) {
+		Tamanio tam = modificar.get();
+		tam.setAlta(false);
+		rpsTamanio.save(tam);
+		}
+	}
+	
+	 @Transactional
+	public Optional<Tamanio> buscarUsuarioPorID(Integer id){
+		Optional<Tamanio> buscar = rpsTamanio.findById(id);
+		return buscar;
+	}
+	
+	 @Transactional
+		public List<Tamanio> buscarTamanio(){
+			List<Tamanio> tamanio = rpsTamanio.findAll();
+			
+			return tamanio;
+		}
+
+	
+	
+	private void validar(String descripcion_tamanio, Integer tamanio, Integer tamanio_precio) throws WebException{
+		
+		if(descripcion_tamanio == null || descripcion_tamanio.isEmpty())  {
+			throw new WebException("la descripcion  no puede estar vacio");
+		}
+		if(tamanio == null) {
+			throw new WebException("el tamanio no puede estar vacio");
+		}
+		
+		if(tamanio_precio ==null ) {
+			throw new WebException("el precio no puede estar vacio");
+		}
+				
+	}
+	
 }
