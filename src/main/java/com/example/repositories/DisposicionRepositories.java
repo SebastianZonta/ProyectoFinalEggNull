7efe9a294d0257befd9419745191e9df350771fe
Bruno Maximino <maximino.bruno92@gmail.com>
2021-11-03 21:33:37 -0300

    Finalizado Disposicioncontroller

--- src/main/java/com/example/repositories/DisposicionRepositories.java
+++ src/main/java/com/example/repositories/DisposicionRepositories.java
@@ -1,11 +1,19 @@
 package com.example.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.entities.Disposicion;
 
+
 @Repository
 public interface DisposicionRepositories extends JpaRepository<Disposicion, Integer>{
-
+	
+	@Query("SELECT a from Perro a WHERE a.activo = true ")
+	public List<Disposicion> buscarActivos();	
+	
 }
+
